99361974d99cbc2bdb82ed7e91a628c727978f37
Bruce Becker <brucellino@protonmail.com>
2022-07-30 01:18:23 +0200

    chore: add vault and digitalocean providers
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- main.tf
+++ main.tf
@@ -4,4 +4,22 @@ terraform {
     scheme  = "http"
     path    = "terraform/digitalocean/hashi-cluster"
   }
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.21.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.8.0"
+    }
+  }
+}
+
+
+provider "vault" {
+
+}
+provider "digitalocean" {
+  # Configuration options
 }
