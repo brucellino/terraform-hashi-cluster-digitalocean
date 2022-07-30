28febeacfb156b7894564974af7d575e906a127f
Bruce Becker <brucellino@protonmail.com>
2022-07-30 02:49:07 +0200

    feat: add vpc and vault modules
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- main.tf
+++ main.tf
@@ -20,6 +20,29 @@ terraform {
 provider "vault" {
 
 }
+data "vault_generic_secret" "do_token" {
+  path = "kv/do"
+}
 provider "digitalocean" {
-  # Configuration options
+  token             = data.vault_generic_secret.do_token.data["token"]
+  spaces_access_id  = data.vault_generic_secret.do_token.data["access_key_hashi_at_home"]
+  spaces_secret_key = data.vault_generic_secret.do_token.data["secret_key_hashi_at_home"]
+}
+
+module "vpc" {
+  source   = "../../../modules/terraform-module-digitalocean-vpc/"
+  vpc_name = "hashi"
+  project = {
+    name        = "Hashi stuff"
+    description = "A hashi cluster (Vault, Consul, Vault) with some nodes"
+    purpose     = "Product development"
+    environment = "development"
+  }
+}
+
+module "vault" {
+  source       = "../../../modules/terraform-module-digitalocean-vault/"
+  depends_on   = [module.vpc]
+  vpc_name     = "hashi"
+  project_name = "Hashi stuff"
 }
