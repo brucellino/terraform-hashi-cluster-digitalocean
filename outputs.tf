07faf1ce120ce86fdb74dd9ddd21df9ee24e11a5
Bruce Becker <brucellino@protonmail.com>
2022-08-02 11:00:58 +0200

    chore: add outputs to the project
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

---
+++ outputs.tf
@@ -0,0 +1,13 @@
+output "server_public_ips" {
+  description = "List of public IPs for the Consul servers"
+  value       = module.consul.server_public_ips
+}
+output "agent_public_ips" {
+  description = "List of public IPs for the Consul agents"
+  value       = module.consul.agent_public_ips
+}
+
+output "lb_ip" {
+  description = "URL of the load balancer fronting the servers"
+  value       = join("", ["http://", module.consul.load_balancer_ip])
+}
