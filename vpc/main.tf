d1545e2995b8024edd0cb802687948e06a6db623
Bruce Becker <brucellino@protonmail.com>
2023-11-01 09:25:08 +0100

    refactor: move configuration to consul
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- vpc/main.tf
+++ vpc/main.tf
@@ -28,12 +28,6 @@ variable "do_vault_mount" {
   sensitive   = false
 }
 
-# variable "cf_vault_mount" {
-#   description = "Path to Vault mount for the Cloudflare secrets."
-#   type        = string
-#   default     = "cloudflare"
-# }
-
 provider "vault" {}
 
 data "vault_kv_secret_v2" "do" {
@@ -41,36 +35,23 @@ data "vault_kv_secret_v2" "do" {
   name  = "tokens"
 }
 
-# data "vault_kv_secret_v2" "cf" {
-#   mount = var.cf_vault_mount
-#   name  = "terraform"
-# }
-# provider "cloudflare" {
-#   api_token = data.vault_kv_secret_v2.cf.data["api_token"]
-# }
-
 provider "digitalocean" {
-  token             = data.vault_kv_secret_v2.do.data["terraform"]
-  spaces_access_id  = data.vault_kv_secret_v2.do.data["spaces_key"]
-  spaces_secret_key = data.vault_kv_secret_v2.do.data["spaces_secret"]
+  token = data.vault_kv_secret_v2.do.data["terraform"]
+  # spaces_access_id  = data.vault_kv_secret_v2.do.data["spaces_key"]
+  # spaces_secret_key = data.vault_kv_secret_v2.do.data["spaces_secret"]
 }
 
 data "consul_keys" "config" {
   key {
-    path = "do"
-    name = "hashi-cluster"
+    path = "do/hashi_cluster"
+    name = "config"
   }
 }
 
 module "vpc" {
-  source   = "brucellino/vpc/digitalocean"
-  version  = "1.0.3"
-  vpc_name = "hashicluster"
-  project = {
-    name        = "HashiDo"
-    description = "Hashi cluster"
-    purpose     = "Personal"
-    environment = "development"
-  }
+  source     = "brucellino/vpc/digitalocean"
+  version    = "1.0.3"
+  vpc_name   = yamldecode(data.consul_keys.config.var.config).vpc_name
+  project    = yamldecode(data.consul_keys.config.var.config).project
   vpc_region = "ams3"
 }
