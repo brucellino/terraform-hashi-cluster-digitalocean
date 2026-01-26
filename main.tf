e2eda56fe43b378ca4c9e9e8d7c3aa8b7b29967d
Bruce Becker <brucellino@protonmail.ch>
2026-01-26 05:45:30 +0100

    fix: ensure that cluster depends on vpc explicitly, with intermediate
    Signed-off-by: Bruce Becker <brucellino@protonmail.ch>

--- main.tf
+++ main.tf
@@ -33,8 +33,16 @@ module "vpc" {
   vpc_description = var.vpc.description
 }
 
+# use a sleep resource to sleep for 30 seconds before destroying vpc
+resource "time_sleep" "wait_after_destroy" {
+  depends_on       = [module.vpc]
+  destroy_duration = "30s"
+}
+
 module "vault" {
+  # This module currently needs access to your ssh to attach to the droplets
   # Creates a vault that must be manually initialized and unsealed
+
   source                   = "brucellino/vault/digitalocean"
   version                  = "~> 3"
   count                    = 1
@@ -42,8 +50,14 @@ module "vault" {
   create_instances         = true
   ssh_inbound_source_cidrs = ["151.74.209.108/24"]
   droplet_size             = "s-1vcpu-1gb"
-  # deploy_zone  = "brusisceddu.xyz"
-  project_name = var.project.name
-  vpc_name     = var.vpc.name
-  depends_on   = [module.vpc]
+  project_name             = var.project.name
+  vpc_name                 = var.vpc.name
+  depends_on               = [module.vpc, time_sleep.wait_after_destroy]
 }
+
+
+
+# Deploy Consul into the Vault cluster
+# resource "ansible_host" "droplets" {
+#   for_each = module.vault.outputs.
+# }
--- terraform.tf
+++ terraform.tf
@@ -5,18 +5,25 @@ terraform {
       version = "~> 4"
     }
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
+      version = "~> 2"
     }
     tailscale = {
-      source = "tailscale/tailscale"
+      source  = "tailscale/tailscale"
+      version = "~> 0.25.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "4.1.0"
+      version = "~> 4"
+    }
+
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.13"
     }
 
   }
-  required_version = "~> 1.11"
+  required_version = "~> 1.14"
   backend "consul" {
     path = "terraform/digitalocean"
   }
